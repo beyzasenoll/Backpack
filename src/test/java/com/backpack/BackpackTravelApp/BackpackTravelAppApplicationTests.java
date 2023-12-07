5150917945c98e8f822ef816668ff0cfc91d8326
beyzasenoll <beyzasenol08@gmail.com>
2023-12-07 23:59:27 +0300

    initial commit

---
+++ src/test/java/com/backpack/BackpackTravelApp/BackpackTravelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backpack.BackpackTravelApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackTravelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
