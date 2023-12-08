810934037e8821e803b45903effb281fbbe88fc1
Root <ruv.ardic@gmail.com>
2023-12-08 17:32:48 +0300

    initialization

---
+++ src/main/java/com/backpack/BackpackTravelApp/BackpackTravelAppApplication.java
@@ -0,0 +1,13 @@
+package com.backpack.BackpackTravelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackTravelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpackTravelAppApplication.class, args);
+	}
+
+}
